3c96f516d65eb3c48465ba949009ac214a255701
hisham_magdi <heshamelsisi99@gmail.com>
2025-06-22 02:50:44 +0300

    Add files via upload

---
+++ PresentationLayer/wwwroot/js/order.js
@@ -0,0 +1,29 @@
+﻿var dataTable
+
+$(document).ready(function () {
+    loadData();
+})
+
+function loadData() {
+    dataTable = $("#mytable").DataTable({
+        "ajax": {
+            "url": "/Admin/Orders/GetData"
+        },
+        "columns": [
+            { "data": "id" },
+            { "data": "applicationUser.name" },
+            { "data": "applicationUser.phoneNumber" },
+            { "data": "applicationUser.email" },
+            { "data": "orderStatus" },
+            { "data": "totalPrice" },
+            
+            {
+                "data": "id",
+                "render": function (data) {
+                    return '<a href="/Admin/Orders/Details?orderid=${data}" class="btn btn-primary" > v dfvDetails</a>'
+                }
+            }
+        ]
+    });
+}
+//<a href="/Admin/Orders/Details?orderid=${data}" class="btn btn-primary" > Details</a>
